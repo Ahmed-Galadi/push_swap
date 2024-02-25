ba8fba8a6fb7a69c70f5ce816e949f4414a4b44d
Ahmed-Galadi <galadiahmed99@gmail.com>
2024-02-25 07:01:06 +0100

    befor we make a function to make an array of ints out of an array of chars

--- is_valid.c
+++ is_valid.c
@@ -6,7 +6,60 @@
 /*   By: agaladi <agaladi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/25 06:44:42 by agaladi           #+#    #+#             */
-/*   Updated: 2024/02/25 06:44:43 by agaladi          ###   ########.fr       */
+/*   Updated: 2024/02/25 06:54:32 by agaladi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+static int		ft_isdigit(char character)
+{
+	return (character >= '0' && character <= '9');
+}
+
+static int		is_norep(int *nbrs)
+{
+	int		i;
+	int		j;
+	int		count;
+
+	count = 0;
+	i = 0;
+	while (nbrs[i])
+	{
+		j = 0;
+		while (nbrs[j])
+		{
+			if (nbrs[i] == nbrs[j])
+				count++;
+			j++;
+		}
+		if (2 <= count)
+			return (0);
+		count = 0;
+		i++;
+	}
+	return (1);
+}
+
+static int 	check_input(char *input)
+{
+	int		i;
+
+	i = 0;
+	while (input[i])
+	{
+		if (!ft_isdigit(input[i]) && input[i] != ' ')
+			return (0);
+		i++;
+	}
+	return (0);
+}
+int 	is_valid_arr(char **args)
+{
+	
+}
+
+int		is_valid_str(char *input)
+{
+}
+
 
--- utils.c
+++ utils.c
@@ -6,57 +6,13 @@
 /*   By: agaladi <agaladi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/24 03:55:11 by agaladi           #+#    #+#             */
-/*   Updated: 2024/02/25 06:17:17 by agaladi          ###   ########.fr       */
+/*   Updated: 2024/02/25 06:59:41 by agaladi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int		ft_isdigit(char character)
-{
-	return (character >= '0' && character <= '9');
-}
-
-int		is_norep(int *nbrs)
-{
-	int		i;
-	int		j;
-	int		count;
-
-	count = 0;
-	i = 0;
-	while (nbrs[i])
-	{
-		j = 0;
-		while (nbrs[j])
-		{
-			if (nbrs[i] == nbrs[j])
-				count++;
-			j++;
-		}
-		if (2 <= count)
-			return (0);
-		count = 0;
-		i++;
-	}
-	return (1);
-}
-
-int 	check_input(char *input)
-{
-	int		i;
-
-	i = 0;
-	while (input[i])
-	{
-		if (!ft_isdigit(input[i]) || input[i] != ' ')
-			return (0);
-		i++;
-	}
-	return (0);
-}
-
-int		*generate_int_tab(char *nbrs)
+int		*str_to_int_tab(char *nbrs)
 {
 	char	**splited_nbrs;
 	int		*nbrs_arr_output;
@@ -75,4 +31,9 @@ int		*generate_int_tab(char *nbrs)
 		i++;
 	}
 	return (free(splited_nbrs),nbrs_arr_output);
+}
+
+int		*args_to_int_tab(char **args)
+{
+	
 }
