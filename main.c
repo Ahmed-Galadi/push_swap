ceb361a958aadf4e707d9d57c5bfd530cd6347d6
Ahmed-Galadi <galadiahmed99@gmail.com>
2024-05-31 02:13:04 +0100

    some changes

--- main.c
+++ main.c
@@ -10,45 +10,41 @@
 
 #include "push_swap.h"
 
-
-
-void	print_list(t_stack *stack, char *name)
+void print_list(t_stack *stack, char *name)
 {
-	t_stack	*current;
+	t_stack *current;
 
 	current = stack;
 	if (!stack)
-		return ;
+		return;
 	printf("\nstack_%s\n", name);
-    while (current != NULL) {
-        printf("%d ", current->content);
-        current = current->next;
-    }
+	while (current != NULL)
+	{
+		printf("%d ", current->content);
+		current = current->next;
+	}
 }
 
-int	main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	int		*input_int_arr;
-	t_stack	*a;
+	int *input_int_arr;
+	t_stack *a;
 	t_stack *b;
-	int		a_len;
+	int a_len;
 
 	input_int_arr = input_formater(argc, argv, &a_len);
 	a = fill_a_stack(input_int_arr, a_len);
 	b = NULL;
 
-
+	sort_all(&a, &b);
 	print_list(a, "a");
-
-	printf("\n%d", a->sorted_index);
-
-
-	while (a) 
+	// free the allocated nodes
+	while (a)
 	{
-        t_stack *temp = a;
-        a = a->next;
-        free(temp);
-    }
+		t_stack *temp = a;
+		a = a->next;
+		free(temp);
+	}
 	free(input_int_arr);
 	return (0);
 }
--- sort100int.c
+++ sort100int.c
@@ -6,34 +6,13 @@
 /*   By: agaladi <agaladi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/05 19:39:33 by agaladi           #+#    #+#             */
-/*   Updated: 2024/05/06 21:04:14 by agaladi          ###   ########.fr       */
+/*   Updated: 2024/05/30 23:55:53 by agaladi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void    set_n(int *n, int stack_size)
+void sort100int(t_stack **stack_a, t_stack **stack_b)
 {
-    if (stack_size <= 10)
-        *n = 5;
-    if (stack_size > 10 && stack_size <= 150)
-        *n = 8;
-    if (stack_size > 150)
-        *n = 18;
-}
-
-void	sort100int(t_stack **stack_a, t_stack **stack_b)
-{
-    int     stack_size;
-	int     n;
-    int     middle;
-    int     offset;
-    int     start;
-    int     end;
-
-    stack_size = list_len(stack_a);
-    set_n(&n, stack_size);
-    offset = stack_size / n;
-    middle = stack_size / 2;
-    
+    return;
 }
